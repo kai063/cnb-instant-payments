0be036c26a9f2a46932f97d185915f233070a58b
Kryštof <krystof.hires@centrum.cz>
2025-07-14 04:40:12 +0200

    zmena path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/cnb-okamzite-platby',
+  trailingSlash: true,
+  output: 'export' // Optional: for static export if needed
 };
 
 export default nextConfig;
